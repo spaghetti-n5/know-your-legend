b9eebcae352d8a68e63068033761452d83f5dae5
Eleonora <eleonora.baret@gmail.com>
2021-12-29 21:46:21 +0100

    added basic filtering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import content from './content/content.json';
+import FilterMenu from './components/FilterMenu';
+import Card from './components/Card';
 
 function App() {
+  const [cards, setCards] = useState(content);
+
+  const handleChange = (val) => {
+    setCards(val);
+    var filteredData = content.filter(function(item) {
+      return item.name === val;
+    });
+    setCards(filteredData);
+  };
+
   return (
     <div>
+      <FilterMenu content={content} changeOption={handleChange} />
       <div className='cardWrap'>
-          {content.map((item) => (
-            <div className='card' key={item.id}>
-              <img src={item.image_preview_url} alt="legendImage" width="100%" />
-              {item.traits.map((trait) => {
-                if (trait.trait_type === "Legend")
-                  return <p key={trait.value}>{item.token_id} - {trait.value}</p>;
-                return null;
-              })}
-            </div>)
-          )}
-        </div>
+        <Card cards={cards} />  
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ cards }) => (
+    <>
+        {cards.map((item) => (
+            <div className='card' key={item.id}>
+              <img src={item.image_preview_url} alt="legendImage" width="100%" />
+              {item.traits.map((trait) => {
+                if (trait.trait_type === "Legend")
+                  return <p key={trait.value}>{item.token_id} - {trait.value}</p>;
+                return null;
+              })}
+            </div>)
+        )}
+    </>
+);
+
+export default Card;
